c2a1447e3fd61757d06da0e6074cdb1cb04e950a
Yuan <49439486+yuanluo2@users.noreply.github.com>
2024-07-08 13:46:38 +0800

    Update extra_funcs.hpp

--- include/extra_funcs.hpp
+++ include/extra_funcs.hpp
@@ -3,7 +3,7 @@
 #include "board.hpp"
 
 namespace cnchess {
-    // every one can only move his pieces, not the enemy's.
+    // every one can only move his/her own pieces, not the enemy's.
     bool check_is_this_your_piece(const ChessBoard& cb, const MoveNode& move, PieceSide side);
 
     // if no one wins, return PS_EXTRA.
